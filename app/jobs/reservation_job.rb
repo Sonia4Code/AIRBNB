7f523bed1773e20fa4d7b48267de804b4a90a26c
SoniaMakhetha <sonia.makhetha@gmail.com>
2018-02-27 14:39:34 +0800

    install sidekiq and redis for background jobs

---
+++ app/jobs/reservation_job.rb
@@ -0,0 +1,9 @@
+class ReservationJob < ApplicationJob
+  queue_as :default
+
+  def perform(customer, host, reservation_id)
+    ReservationMailer.booking_email(customer, host, reservation_id)
+    # Do something later
+
+  end
+end
